895bd3b44d229e83b63174583c7dc6d941f8c526
invalid-code <jessgaming55@gmail.com>
2025-05-11 18:17:31 +0800

    Hidden sensitive secrets

--- my_blog_website/settings.py
+++ my_blog_website/settings.py
@@ -10,7 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+#a1bv&19*42gbie3ns#ki=b#x85^d*(3tqwjic*roq*vu3w=0'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("IS_DEBUG")
 
 ALLOWED_HOSTS = []
 
